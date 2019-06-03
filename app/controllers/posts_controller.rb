58c59914f57dd738ae167eddcafab910459cdd71
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2019-06-03 10:01:42 +0200

    Implement create view and destroy for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all.map(&:created_at)
+  end
+
+  def create
+    current_user.posts.create!(post_params)
+    redirect_to :posts
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def destroy
+    Post.destroy(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:resource)
+  end
 end
