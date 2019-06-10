1d7fec5532409472a8679ea12fff5a0ecefc6134
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2019-06-10 17:45:45 +0200

    Allow viewing post details unauthenticated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < AuthenticationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @posts = Post.all
