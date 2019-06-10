841d49624a67812b6f7a6e6556d6379bed29e67d
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2019-06-10 15:18:22 +0200

    Improve user posts query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @all_posts = Post.all;
-    @posts = @all_posts.select { |post| post.user_id == @user.id }
+    @posts = @user.posts
   end
 end
