7cc5b8b58d1aafa87b8c642a063ad884beeb7483
Tristan Schönhals <tristan.schoenhals@mni.thm.de>
2019-06-25 17:05:07 +0200

    Fix prev and next needing authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < AuthenticationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :next, :prev]
 
   def index
     filter = params[:filter] || :open
